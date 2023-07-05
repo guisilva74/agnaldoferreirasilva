a252c1f77aaae22b19750272e05871af1963f885
guisilva74 <agnaldosilva@sapo.pt>
2023-07-05 22:03:15 +0000

    subir site para cima

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+<body>
+   
+<?php 
+
+
+echo "Segurança/ Vigilante profissional, com cartão emitido pela PSP"
+
+?>
+
+
+
+</body>
+</html>
+  
+  
+
